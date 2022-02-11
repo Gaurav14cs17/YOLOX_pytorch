a090e202d78f2308ae9efbdc72450bfe5114f459
Gaurav14cs17 <gouravkumar717@gmail.com>
2022-02-11 20:00:00 +0530

    mix_pre

--- temp.py
+++ temp.py
@@ -1,1 +1,15 @@
-s
+import os
+import shutil
+
+
+path = "D:\labs\object_detection_model\old\Yolo-FastestV2\img"
+output_path = ".\Riya"
+
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
+for x in os.listdir(path):
+    full_path = os.path.join(path , x )
+    print(full_path)
+    #shutil.copy(full_path , output_path  )
+    #shutil.move( s , d)
